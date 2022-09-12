47d769996a4406048bbd7ad7db572615b51e575c
Oleksandr-Pustovit <Pustovit.o2020@gmail.com>
2022-09-12 09:47:03 +0300

    My-bike-landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#navMenu') {
+    document.body.classList.add('body--with-menu');
+  } else {
+    document.body.classList.remove('body--with-menu');
+  }
+});
+
+const form = document.querySelector('.contact-us__form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+  window.scrollTo(0, 0);
+});
